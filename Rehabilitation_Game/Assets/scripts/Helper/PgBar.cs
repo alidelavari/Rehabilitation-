48042fc38a3906177e77e7e81d7c745495f0b387
Ali Delavari <alidelavari1378@gmail.com>
2020-07-28 13:37:23 +0430

    helper scene is created fo progress bar
    Signed-off-by: Ali Delavari <alidelavari1378@gmail.com>

---
+++ Rehabilitation_Game/Assets/scripts/Helper/PgBar.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PgBar : MonoBehaviour
+{
+    [SerializeField] float pgTarget = 0.5f;
+    [SerializeField] float rate = 0.1f;
+    RectTransform rectTransform;
+    public PgLabel pgLabel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject pg_label = GameObject.Find("pg_label");
+        pgLabel = (PgLabel)pg_label.GetComponent(typeof(PgLabel));
+        transform.localScale = new Vector2(0, 1);
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.localScale.x < pgTarget)
+            StartToMove();
+    }
+
+    void StartToMove()
+    {
+        float scale = rate * Time.deltaTime + transform.localScale.x;
+        transform.localScale = new Vector2(scale, 1);
+        pgLabel.SetPos(transform.position.x + rectTransform.rect.width * transform.localScale.x);
+    }
+}
---
+++ Rehabilitation_Game/Assets/scripts/Helper/PgLabel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PgLabel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetPos(float x)
+    {
+        transform.position = new Vector2(x, transform.position.y);
+    }
+}
