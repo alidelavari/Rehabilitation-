2957f40beaeb6e0334ec7947951c4406233ffbf4
Ali Delavari <alidelavari1378@gmail.com>
2020-08-09 00:14:41 +0430

    add : progress bar and angle measurement fix : bug of sounds add : background

---
+++ Rehabilitation_Game/Assets/scripts/Helper/pgCircle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pgCircle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float angle = transform.rotation.eulerAngles.z;
+        if (angle > 180)
+            angle = 0;
+        transform.rotation = Quaternion.Euler(0, 0, angle + 1);
+    }
+}
